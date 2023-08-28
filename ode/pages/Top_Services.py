869df83025f5c51bc33b9e3627b5daf86afb806a
ATHARVA SHELKE <122624700+aasuraK@users.noreply.github.com>
2023-08-28 19:06:41 +0530

    Update Top_Services.py

--- ode/pages/Top_Services.py
+++ ode/pages/Top_Services.py
@@ -3,7 +3,7 @@ import streamlit as st
 import seaborn as sb
 import matplotlib.pyplot as plt
 
-filepath2= r"D:/topcombinations1.csv"
+filepath2= r"topcombinations1.csv"
 data = pd.read_csv(filepath2)
 
 a = st.text_input("How many top combinations do you want", value='5')  # Default value set to 10
